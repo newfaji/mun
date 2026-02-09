5c62fe0e7d68581d68732d70b7ae24b1433850b2
newfaji <158270331+newfaji@users.noreply.github.com>
2026-02-09 16:55:35 +0900

    260209 삼성꿈장학재단

--- sdream/js/main.js
+++ sdream/js/main.js
@@ -8,4 +8,38 @@ $(document).ready(function(){
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
     });//visual_swiper
 
+    /****************************************
+     *  header에 마우스를 올리면(오버하면) over 클래스를 추가
+     *           마우스를 내리면(아웃하면) over 클래스 삭제
+     *  브라우저의 스크롤을 내리면 header에 fixed 클래스를 추가
+     *  브라우저를 다시 맨위로 스크롤 하면 fixed 클래스를 삭제
+    ****************************************/
+    $('.header').on('mouseenter', function(){
+        //console.log('마우스 올렸다!!!!')
+        $(this).addClass('over')
+    })
+    $('.header').on('mouseleave', function(){
+        //console.log('마우스를 올렸다 내렸다!!!!')
+        $(this).removeClass('over')
+    })
+
+    let scrolling //브라우저가 스크롤 된 값
+
+    function header_fixed(){
+        scrolling = $(window).scrollTop()
+        console.log(scrolling)
+        if(scrolling > 0){
+            //console.log('0보다 크다')
+            $('.header').addClass('fixed')
+        }else{
+            //console.log('0이거나 0보다 작다')
+            $('.header').removeClass('fixed')
+        }
+    }
+    //맨 처음 html이 로딩된 이후 단 한번실행
+    header_fixed() 
+    $(window).scroll(function(){ //브라우저가 실행될때마다 실행
+        header_fixed() 
+    })
+
 })//$(document).ready
