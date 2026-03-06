f7d9f21b1349ddb8a3b9670517088713b9c1e358
newfaji <158270331+newfaji@users.noreply.github.com>
2026-03-06 18:06:09 +0900

    .0

--- animal_260305/js/common.js
+++ animal_260305/js/common.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
         } else {
             device_status = "mo";
         }
-        console.log(device_status);
+        //console.log(device_status);
     }
 
     device_chk(); //문서가 로딩되었을때 단 1번 실행
@@ -94,6 +94,27 @@ $(document).ready(function () {
         if (device_status == "mo") {
              $('.header').removeClass('menu_open')
         }
+    })
+
+
+    /* top버튼을 클릭하면 상단으로 스크롤 */
+    $('.footer .top').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500)
+    })
+
+    /* footer의 높이를 계산해서 container의 하단 여백으로 줌 */
+    let footer_h //foter 높이
+
+    function footer_count(){ //함수의 정의
+        footer_h = $('.footer').outerHeight()
+        console.log(footer_h)
+        $('.container').css('margin-bottom', footer_h)
+    }
+    footer_count() //함수실행(호출), html이 로딩된 이후 1번만
+    $(window).resize(function(){
+        footer_count() //함수실행(호출), 브라우저가 리사이즈 될때마다 1번씩
     })
 
 });
--- animal_260305/js/main.js
+++ animal_260305/js/main.js
@@ -1,7 +1,7 @@
 /*************************
 * main.js는 메인페이지에서만 구동되는 스크립트를 저장..
 ***********************/
-$(document).ready(function(){
+$(document).ready(function () {
     $('.visual .popup_wrap .popup').slick({
         autoplay: true, //팝업 자동 실행
         autoplaySpeed: 5000, //팝업이 머무는 시간
@@ -13,12 +13,12 @@ $(document).ready(function(){
         //infinite: false, //무한반복
     });
 
-    $('.visual .ctrl_wrap .stop').on('click', function(){
+    $('.visual .ctrl_wrap .stop').on('click', function () {
         $('.visual .popup_wrap .popup').slick('slickPause');  /* 일시정지 기능 */
         $(this).hide()
         $('.visual .ctrl_wrap .play').show()
     })
-    $('.visual .ctrl_wrap .play').on('click', function(){
+    $('.visual .ctrl_wrap .play').on('click', function () {
         $('.visual .popup_wrap .popup').slick('slickPlay');  /* 재생 기능 */
         $(this).hide()
         $('.visual .ctrl_wrap .stop').show()
@@ -38,7 +38,7 @@ $(document).ready(function(){
     let tab_list = $('.find .tab_list ul li')
     let tab_name
 
-    tab_list.on('click', function(){
+    tab_list.on('click', function () {
         tab_list.removeClass('active')
         tab_list.attr('aria-selected', 'false')
         tab_list.find('button em').text('')
@@ -48,7 +48,7 @@ $(document).ready(function(){
         tab_name = $(this).attr('aria-controls')
         //console.log(tab_name)
         $('.find .tab_conts .tab_item').removeClass('active')
-        $('.find .tab_conts').find('#'+tab_name).addClass('active')
+        $('.find .tab_conts').find('#' + tab_name).addClass('active')
 
         $('.find .tab_conts .tab_item.active .tab_conts_list').slick('setPosition')
     })
@@ -122,7 +122,7 @@ $(document).ready(function(){
         ]
     });
 
-    
+
     /**********************************
      * message는 높이가 높고 그 안에 inner가 고정으로 있음 sticky
      * message가 스크롤 되는 동안 p strong span태그가 넓이가 넓어지면서 색상 바뀜
@@ -146,28 +146,28 @@ $(document).ready(function(){
     //3번째 strong의 span 넓이 50%
     //color_obj.eq(2).find(color_resizer).width('50%')
 
-    function color_change(){
+    function color_change() {
         scrolling = $(window).scrollTop()
         color_win_h = $(window).height()
         color_header = $('.header').height()
 
-        color_start = color_area.offset().top - color_header      
+        color_start = color_area.offset().top - color_header
         color_end = color_area.offset().top + color_area.height() - color_win_h
-        
-        if(color_end < scrolling){
-            console.log('끝')
+
+        if (color_end < scrolling) {
+            //console.log('끝')
             color_obj.find(color_resizer).width('100%')
-        }else if(color_start > scrolling){
-            console.log('시작전')
+        } else if (color_start > scrolling) {
+            // console.log('시작전')
             color_obj.find(color_resizer).width('0')
-        }else{
+        } else {
             color_total = color_end - color_start
             color_diff = scrolling - color_start
             color_count = color_diff / color_total * 100
-            console.log(color_count)
-            for(i=0; i<color_line; i++){
-                color_w = (color_count - (100/color_line) * i) * color_line
-                if(color_w > 100){
+            // console.log(color_count)
+            for (i = 0; i < color_line; i++) {
+                color_w = (color_count - (100 / color_line) * i) * color_line
+                if (color_w > 100) {
                     color_w = 100
                 }
                 color_obj.eq(i).find(color_resizer).width(color_w + '%')
@@ -175,8 +175,69 @@ $(document).ready(function(){
         }
     }
     color_change() //문서가 로딩되고 단 1번 실행
-    $(window).scroll(function(){ //스크롤 될때마다 1번씩 실행
+    $(window).scroll(function () { //스크롤 될때마다 1번씩 실행
         color_change()
     })
+
+
+    //adopt의 slick
+    $('.adopt .list_wrap .list').slick({
+        autoplay: false, //팝업 자동 실행
+        autoplaySpeed: 3000, //팝업이 머무는 시간
+        speed: 500, //팝업 전환 속도
+        dots: false, //하단 페이지 버튼 (true, false)
+        arrows: false,  //다음, 이전팝업 (true, false)
+        //pauseOnHover: true, //마우스호버시 일시정지
+        infinite: true, //무한반복
+        variableWidth: true, //넓이를 자유롭게 설정
+        slidesToShow: 6, //한번에 보일 팝업 수
+        //slidesToScroll: 1, //한번 드래그에 움직이는 슬라이드 제한
+        swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+        centerMode: true, //가운데정렬(가운데가 1번)
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    centerMode: false, //가운데정렬(가운데가 1번)
+                }
+            },
+        ]
+    });
+
+    //review slick 
+    $('.review .list_wrap .list').slick({
+        autoplay: false, //팝업 자동 실행
+        autoplaySpeed: 3000, //팝업이 머무는 시간
+        speed: 500, //팝업 전환 속도
+        dots: false, //하단 페이지 버튼 (true, false)
+        arrows: false,  //다음, 이전팝업 (true, false)
+        //pauseOnHover: true, //마우스호버시 일시정지
+        //infinite: false, //무한반복
+        //variableWidth: true, //넓이를 자유롭게 설정
+        slidesToShow: 4, //한번에 보일 팝업 수
+        //slidesToScroll: 1, //한번 드래그에 움직이는 슬라이드 제한
+        swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+        //centerMode: true, //가운데정렬(가운데가 1번)
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+        ]
+    });
 
 })//document.ready
