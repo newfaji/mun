2d85f9f4ce2f886e4df46c7aae592c37fe831baa
newfaji <158270331+newfaji@users.noreply.github.com>
2026-03-13 16:56:01 +0900

    .0

--- forest/js/main.js
+++ forest/js/main.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 			}else if(destination.index == 2){
 				//console.log('3번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg', 'blue')
-				$('.header').attr('data-bg', '')
+				$('.header').attr('data-bg', 'mo')
 			}else if(destination.index == 3){
 				//console.log('4번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg', 'white')
@@ -69,6 +69,24 @@ $(document).ready(function(){
 	/* visual의 swiper 끝 */
 
 
-	
+	/* biz의 swiper 시작 */
+	const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+
+			768: {    /* 640px 이상일때 적용 */
+				slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+			1280: {    /* 640px 이상일때 적용 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	});
+	/* biz의 swiper 끝 */
 
 })//$(document).ready
